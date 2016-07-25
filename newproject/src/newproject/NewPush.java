a1882579a7d3003f01551f4d2b5ef22f9d2961f1
krishnbharathi <krishnbharathi@gmail.com>
2016-07-25 11:38:36 +0530

    dfsdf

--- newproject/src/newproject/NewPush.java
+++ newproject/src/newproject/NewPush.java
@@ -5,7 +5,8 @@ public class NewPush {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("done");
-
+		System.out.println("done");
+System.out.println("done");
 System.out.println("done");
 System.out.println("done");
 System.out.println("done");
